ba3ef0c968b283e7eb048615cd47f602e36b39ae
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-26 15:07:42 -0800

    Add my_each enumerable

---
+++ my_each.rb
@@ -0,0 +1,13 @@
+module Enumerable
+  def my_each
+    for i in 0..self.length - 1
+      yield self[i]
+    end
+    self
+  end
+end
+
+puts 'my_each vs. each'
+numbers = [1, 2, 3, 4, 5]
+numbers.my_each  { |item| puts item }
+numbers.each  { |item| puts item }
