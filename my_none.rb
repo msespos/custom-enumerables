b56ccd1f4c2a2912dfd09dce22f87e6e5df438c5
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-26 18:49:53 -0800

    Add my_none? enumerable

---
+++ my_none.rb
@@ -0,0 +1,27 @@
+module Enumerable
+  def my_each
+    for i in 0..self.length - 1
+      yield self[i]
+    end
+    self
+  end
+
+  def my_none?
+    selected = []
+    self.my_each do |element|
+      return false if yield element
+    end
+    true    
+  end
+end
+
+puts 'my_none? vs. none?'
+fibs1 = [0, 1, 1, 2, 3]
+fibs2 = [5, 8, 13, 21, 34]
+empty = []
+puts fibs1.my_none? { |number| number > 5 }
+puts fibs1.none? { |number| number > 5 }
+puts fibs2.my_none? { |number| number > 5 }
+puts fibs2.none? { |number| number > 5 }
+puts empty.my_none? { |number| number > 5 }
+puts empty.none? { |number| number > 5 }
