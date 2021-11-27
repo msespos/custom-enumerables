25b513d7c49a1c6b542ef5975054857511a069d1
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-27 14:39:59 -0800

    Add my_map enumerable method

---
+++ my_map.rb
@@ -0,0 +1,23 @@
+module Enumerable
+  def my_each
+    for i in 0..self.length - 1
+      yield self[i]
+    end
+    self
+  end
+
+  def my_map
+    elements = []
+    self.my_each do |element|
+      elements << yield(element)
+    end
+    elements
+  end
+end
+
+puts 'my_each vs. each'
+numbers = [1, 2, 3, 4, 5]
+puts numbers.my_map { |item| item ** 2 }
+puts numbers.map { |item| item ** 2 }
+numbers.my_map { |item| item ** 2 }
+numbers.map { |item| item ** 2 }
