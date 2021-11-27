554c64baa65dd91af5e4d3f2405aee14a6358f96
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-26 18:35:30 -0800

    Add my_all? enumerable

---
+++ my_all.rb
@@ -0,0 +1,27 @@
+module Enumerable
+  def my_each
+    for i in 0..self.length - 1
+      yield self[i]
+    end
+    self
+  end
+
+  def my_all?
+    selected = []
+    self.my_each do |element|
+      return false if !yield element
+    end
+    true    
+  end
+end
+
+puts 'my_all? vs. all?'
+fibs1 = [0, 1, 1, 2, 3]
+fibs2 = [5, 8, 13, 21, 34]
+empty = []
+puts fibs1.my_all? { |number| number > 0 }
+puts fibs1.all? { |number| number > 0 }
+puts fibs2.my_all? { |number| number > 0 }
+puts fibs2.all? { |number| number > 0 }
+puts empty.my_all? { |number| number > 0 }
+puts empty.all? { |number| number > 0 }
