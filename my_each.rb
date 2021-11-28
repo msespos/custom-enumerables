e3a5145888f7671ee55a1b153f4efc07b336c18d
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-28 12:44:29 -0800

    Add functionality for hashes

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,13 @@
 module Enumerable
   def my_each
-    for i in 0..self.length - 1
-      yield self[i]
+    if self.class == Array
+      for i in 0..self.length - 1
+        yield self[i]
+      end
+    else
+      for key, self[key] in self
+        yield key, self[key]
+      end
     end
     self
   end
@@ -9,5 +15,8 @@ end
 
 puts 'my_each vs. each'
 numbers = [1, 2, 3, 4, 5]
+numbers_hash = { one: 1, two: 2, three: 3 }
 numbers.my_each { |item| puts item }
 numbers.each { |item| puts item }
+numbers_hash.my_each { |key, value| puts key, value }
+numbers_hash.each { |key, value| puts key, value }
