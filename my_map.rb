bf6324ba563fa5301c698d7027b5ab89ae1b948f
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-28 18:27:02 -0800

    Edit my_map to take a block and a proc

--- my_map.rb
+++ my_map.rb
@@ -6,10 +6,17 @@ module Enumerable
     self
   end
 
-  def my_map
-    elements = []
-    self.my_each do |element|
-      elements << yield(element)
+  def my_map(*args)
+    if args == []
+      elements = []
+      self.my_each do |element|
+        elements << yield(element)
+      end
+    else
+      elements = []
+      self.my_each do |element|
+        elements << args[0].call(element)
+      end
     end
     elements
   end
@@ -17,7 +24,18 @@ end
 
 puts 'my_map vs. map'
 numbers = [1, 2, 3, 4, 5]
+my_proc = Proc.new { |item| item ** 2 }
 puts numbers.my_map { |item| item ** 2 }
 puts numbers.map { |item| item ** 2 }
+puts numbers.my_map(&my_proc)
+puts numbers.map(&my_proc)
+puts 'my_map takes a proc'
+puts numbers.my_map(my_proc)
+puts 'my map takes a proc and a block'
+puts numbers.my_map(my_proc) { |item| item ** 2 }
 numbers.my_map { |item| item ** 2 }
 numbers.map { |item| item ** 2 }
+numbers.my_map(&my_proc)
+numbers.map(&my_proc)
+numbers.my_map(my_proc)
+
